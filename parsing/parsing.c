0795becb6d455ab0687bf545e9e1dbe6d43e8dcc
felixmustin <60741148+felixmustin@users.noreply.github.com>
2022-03-23 14:21:14 +0100

    modif parsing

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -1,11 +1,24 @@
 #include "../minishell.h"
 
-
-struct s_cmd parse_cmds(t_list *lst)
+t_cmd get_pipe(t_cmd full_cmd, t_token *token)
 {
+    if (token->type == pipeline)
+        full_cmd.type_pipe = 0; 
+    if (token->type == and)
+        full_cmd.type_pipe = 1;
+    if (token->type == or)
+        full_cmd.type_pipe = 2;
+    full_cmd.pipe_o = 1;
+}
+
+struct s_cmd parse_cmds(t_list *lst, int i)
+{
+
     t_cmd full_cmd;
 
     init_struct(&full_cmd);
+    if (i > 0)
+        full_cmd.pipe_i = 1;
     while(lst)
     {
         if (lst->token->type == literal)
@@ -19,7 +32,7 @@ struct s_cmd parse_cmds(t_list *lst)
         else if (lst->token->type == double_redir_left)
             full_cmd.fd_i = get_redir(lst->token);
         else if (lst->token->type == (pipeline || and || or))
-            return(get_pipe_or_op(lst->token)); //function set int in struct
+            return(get_pipe(full_cmd, lst->token));
         lst = lst->next;
     }
     return (full_cmd);
@@ -35,7 +48,7 @@ int interpreter(t_list *lst, t_all_cmd *all_cmd)
     i = 0;
     while (i < all_cmd->nbrcmd)
     {
-        all_cmd->cmds[i] = parse_cmds(lst);
+        all_cmd->cmds[i] = parse_cmds(lst, i);
         i++;
     }
 }
