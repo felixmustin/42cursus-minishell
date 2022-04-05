f08e63ee5b9fa415c72e696ac7fc2c7e81eb7f5b
felixmustin <60741148+felixmustin@users.noreply.github.com>
2022-04-05 21:47:35 +0200

    prompt

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -2,6 +2,7 @@
 
 char *parse_cwd(char *cwd)
 {
+    char *last_cwd;
     char *cut_cwd;
     int i;
     int j;
@@ -18,16 +19,18 @@ char *parse_cwd(char *cwd)
         cut_cwd[j++] = cwd[i];
     cut_cwd[j++] = ' ';
     cut_cwd[j] = 0;
-    return (cut_cwd);   
+    last_cwd = ft_strjoin("\U0001F449 ", cut_cwd);
+    free(cut_cwd);
+    return (last_cwd);   
 }
 
 char *new_prompt(void)
 {
     char *cwd;
-    char *cut_cwd;
+    char *last_cwd;
 
     cwd = getcwd(NULL, 0);
-    cut_cwd = parse_cwd(cwd);
+    last_cwd = parse_cwd(cwd);
     free(cwd);
-    return (cut_cwd);
+    return (last_cwd);
 }
