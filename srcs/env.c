8bafdbc83a6602f8965d3b268d98f7335a7a8853
felixmustin <60741148+felixmustin@users.noreply.github.com>
2022-04-21 14:23:09 +0200

    norm

--- srcs/env.c
+++ srcs/env.c
@@ -1,42 +1,42 @@
 #include "../includes/minishell.h"
 
-int env_len(char **envv)
+int	env_len(char **envv)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (envv[i])
-        i++;
-    return (i);
+	i = 0;
+	while (envv[i])
+		i++;
+	return (i);
 }
 
-void free_env(char **envv)
+void	free_env(char **envv)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    j = 0;
-    i = env_len(envv);
-    while(j < i)
-    {
-        free(envv[j]);
-        envv[j] = NULL;
-        j++;
-    }
-    free(envv);
-    envv = NULL;
+	j = 0;
+	i = env_len(envv);
+	while (j < i)
+	{
+		free(envv[j]);
+		envv[j] = NULL;
+		j++;
+	}
+	free(envv);
+	envv = NULL;
 }
 
-void init_env(char **envv)
+void	init_env(char **envv)
 {
-    int i;
+	int	i;
 
-    env = malloc(sizeof(char *) * (env_len(envv) + 1));
-    i = 0;
-    while (envv[i])
-    {
-        env[i] = ft_strdup(envv[i]);
-        i++;
-    }
-    env[i] = 0;
+	env = malloc(sizeof(char *) * (env_len(envv) + 1));
+	i = 0;
+	while (envv[i])
+	{
+		env[i] = ft_strdup(envv[i]);
+		i++;
+	}
+	env[i] = 0;
 }
--- srcs/prompt.c
+++ srcs/prompt.c
@@ -1,42 +1,42 @@
 #include "../includes/minishell.h"
 
-char *parse_cwd(char *cwd)
+char	*parse_cwd(char *cwd)
 {
-    char 	*last_cwd;
-    char 	*cut_cwd;
+	char	*last_cwd;
+	char	*cut_cwd;
 	char	*mid_cwd;
 	char	*home;
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = ft_strlen(cwd);
-    j = i;
-    while(cwd[i] != '/')
-        i--;
-    cut_cwd = malloc(sizeof(char) * (j - i) + 1);
-    if (!cut_cwd)
-        return (NULL);
-    j = 0;
-    while (cwd[++i])
-        cut_cwd[j++] = cwd[i];
-    cut_cwd[j++] = ' ';
-    cut_cwd[j] = 0;
+	i = ft_strlen(cwd);
+	j = i;
+	while (cwd[i] != '/')
+		i--;
+	cut_cwd = malloc(sizeof(char) * (j - i) + 1);
+	if (!cut_cwd)
+		return (NULL);
+	j = 0;
+	while (cwd[++i])
+		cut_cwd[j++] = cwd[i];
+	cut_cwd[j++] = ' ';
+	cut_cwd[j] = 0;
 	mid_cwd = ft_strjoin(RED, cut_cwd);
-    home = ft_strjoin("\U0001F449 ", mid_cwd);
+	home = ft_strjoin("\U0001F449 ", mid_cwd);
 	last_cwd = ft_strjoin(home, DEFAULT);
-    free(cut_cwd);
+	free(cut_cwd);
 	free(home);
 	free(mid_cwd);
-    return (last_cwd);   
+	return (last_cwd);
 }
 
-char *new_prompt(void)
+char	*new_prompt(void)
 {
-    char *cwd;
-    char *last_cwd;
+	char	*cwd;
+	char	*last_cwd;
 
-    cwd = getcwd(NULL, 0);
-    last_cwd = parse_cwd(cwd);
-    free(cwd);
-    return (last_cwd);
+	cwd = getcwd(NULL, 0);
+	last_cwd = parse_cwd(cwd);
+	free(cwd);
+	return (last_cwd);
 }
